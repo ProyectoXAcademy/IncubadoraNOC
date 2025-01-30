a5be944e9f0dc033b562df65efd483d793ae6e65
Gonzalo Miranda <gonzalo.miranda0912@gmail.com>
2025-01-30 20:43:20 -0300

    Creacion de primer modelo User

---
+++ back/Models/UserModels.js
@@ -0,0 +1,39 @@
+import {DataTypes} from 'sequelize'
+import {dbConfig} from '../Config'
+
+const UserModel = dbConfig.sequelize.define('User', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dni: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    date_of_birth: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+})
+
+module.exports = {UserModel}
