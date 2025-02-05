375cba0a48930680e7e59ea819ac58c6e8f07496
franmarg92 <gabrielleschifranco@gmail.com>
2025-02-04 22:05:20 -0300

    subiendo carrusel

--- front/src/app/views/home/home.component.ts
+++ front/src/app/views/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
+  currentIndex: number = 0;
+  totalSlides: number = 3;
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
+  }
+
+  prevSlide(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.totalSlides) % this.totalSlides;
+  }
+
+  goToSlide(index: number): void {
+    this.currentIndex = index;
+  }
+
+  ngOnInit() {
+    // Mueve las imágenes cada 3 segundos (3000ms)
+    setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
 
 }
