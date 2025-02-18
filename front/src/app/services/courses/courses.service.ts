5b238158d031c31f0e37956d6070d23a802c8874
“Nahuel <nnahuell.gb@hotmail.com>
2025-02-18 07:06:14 -0300

    mi cursos

--- front/src/app/services/courses/courses.service.ts
+++ front/src/app/services/courses/courses.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Courses } from '../../models/courses.model';
 
 @Injectable({
@@ -8,20 +8,28 @@ import { Courses } from '../../models/courses.model';
 })
 export class CoursesService {
 
- // agregar endpoint
- endpoint_create_courses:string = "http://localhost:3000/api/course/create"
- endpoint_get_courses: string =  "http://localhost:3000/api/course"
+  // Endpoints
+  private endpoint_create_courses: string = "http://localhost:3000/api/course/create";
+ // Endpoint base de cursos
+ private endpoint_get_courses: string = "http://localhost:3000/api/course/"; 
 
+  constructor(private http: HttpClient) { }
 
- constructor( private http:HttpClient) { }
-
- createCoursePOST(course:Courses):Observable<any>{
-      return this.http.post(this.endpoint_create_courses,course)
- }
-
-  // Método para obtener los cursos (GET)
-  getCoursesGET(): Observable<Courses[]> {
-    return this.http.get<Courses[]>(this.endpoint_get_courses);
+  // Método para crear un curso (POST)
+  createCoursePOST(course: Courses): Observable<any> {
+    return this.http.post(this.endpoint_create_courses, course);
   }
+
+// Método para obtener los cursos del usuario logueado usando solo el token
+getCourses(): Observable<Courses[]> {
+  const token = localStorage.getItem('token'); // Obtener el token del usuario
+
+  if (!token) {
+    return throwError(() => new Error("No se encontró el token en localStorage"));
+  }
+
+  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`); // Enviar el token en los headers
+  return this.http.get<Courses[]>(this.endpoint_get_courses, { headers });
 }
+} 
 
