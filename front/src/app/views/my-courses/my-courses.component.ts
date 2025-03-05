a82c20e7be1a5c736e6dd4da1e7ba2106d1c6e67
“Nahuel <nnahuell.gb@hotmail.com>
2025-03-05 20:42:09 -0300

    end2

--- front/src/app/views/my-courses/my-courses.component.ts
+++ front/src/app/views/my-courses/my-courses.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../../services/courses/courses.service';
+import { Courses } from '../../models/courses.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-courses',
-  imports: [],
-  standalone:true,
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './my-courses.component.html',
-  styleUrl: './my-courses.component.css'
+  styleUrls: ['./my-courses.component.css']
 })
-export class MyCoursesComponent {
+export class MyCoursesComponent implements OnInit {
+  courses: Courses[] = [];
 
+  constructor(private coursesService: CoursesService) {}
+
+  ngOnInit() {
+    this.loadUserCourses();
+  }
+
+  loadUserCourses() {
+    // Llamar al método correctamente con ()
+    this.coursesService.getCoursesGET().subscribe({
+      next: (data) => {
+        this.courses = data;
+      },
+      error: (error) => {
+        console.error('Error al obtener los cursos del usuario', error);
+      }
+    });
+  }
 }
