dd1b1953f5a365c00d02213a1add592bcc5e4e34
“Nahuel <nnahuell.gb@hotmail.com>
2025-03-09 11:10:38 -0300

    header add class dashboard and logout for login test

--- testing/cypress/e2e/loginTest.cy.js
+++ testing/cypress/e2e/loginTest.cy.js
@@ -1,4 +1,4 @@
-const usersLogin = require('../fixtures/loginEditado.json'); // Asegúrate de que la ruta sea correcta
+const usersLogin = require('../fixtures/login.json'); // Asegúrate de que la ruta sea correcta
 
 describe('incubadoraNoc Test demo login', { testIsolation: false }, () => {
     usersLogin.forEach(user => {
--- testing/cypress/support/commands.js
+++ testing/cypress/support/commands.js
@@ -1,22 +1,16 @@
 // <- HERE IS THE OTHER CLICKS -> 
 // GO TO DASHBOARD
 Cypress.Commands.add('goToDashboard', () => {
-    cy.get('body').then($body => {
-        // Verificar si el botón de logout existe dentro del dropdown
-        if ($body.find('.dropdown-btn').length > 0) {
-            // Abrir el menú desplegable
-            cy.get('.dropdown-btn').should('be.visible').click();
+    // Asegurar que el dropdown esté visible y hacer clic
+    cy.get('.dropdown-btn').should('be.visible').click();
 
-            // Asegurar que el menú se muestre
-            cy.get('.dropdown-menu').invoke('show');
+    // Forzar la visibilidad del menú manualmente si sigue oculto
+    cy.get('.dropdown-menu').invoke('show');
 
-            // Ahora hacer clic en la opción "Ir al Dashboard"
-            cy.contains('a', 'Ir al Dashboard').should('be.visible').click();
+    // Ahora hacer clic en la opción "Ir al Dashboard"
+    cy.contains('a', 'Ir al Dashboard').should('be.visible').click();
 
-            // Validar que la URL cambia a la página de login
-            cy.url().should('include', '/dashboard');
-        } else {
-            cy.log('El usuario no inició sesión, no es necesario ir al dashboard.');
-        }
-    });
+    // Verificar que la URL cambió
+    cy.url().should('include', '/dashboard');
 });
+
