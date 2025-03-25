d1697631215ef9e78aa6988dfca8edb016c3caa8
“Nahuel <nnahuell.gb@hotmail.com>
2025-03-25 10:16:16 -0300

    new styless

--- front/src/app/core/publications/publications.component.ts
+++ front/src/app/core/publications/publications.component.ts
@@ -2,30 +2,44 @@ import { Component, Input } from '@angular/core';
 import {NgFor} from '@angular/common'
 import { Publication } from '../../models/publication.model';
 import { PublicationsService } from '../../services/publications/publications.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-publications',
   imports: [NgFor],
   standalone: true,
   templateUrl: './publications.component.html',
-  styleUrl: './publications.component.css'
+  styleUrl: './publications.component.css',
+  providers: [DatePipe]
+
 })
 export class PublicationsComponent {
-   constructor(private serv:PublicationsService){}
-  
+  constructor(private serv: PublicationsService, private datePipe: DatePipe) {}
+
   publications: Publication[] = [];
 
-ngOnInit(){
-  // DESCOMENTAR UNA VEZ HAYA PUBLICACIONES EN EL BACK
-  this.publicationsGET()
-}//
+  ngOnInit() {
+    // DESCOMENTAR UNA VEZ HAYA PUBLICACIONES EN EL BACK
+    this.publicationsGET();
+  }
 
- publicationsGET(){
-  this.serv.publicationsGet().subscribe(
-    (data:any) =>  { this.publications = data
-    },
-    (error) => {console.log("error al traer publicaciones")}
-  )
- }
+  publicationsGET() {
+    this.serv.publicationsGet().subscribe(
+      (data: any) => {
+        this.publications = data;
+      },
+      (error) => {
+        console.log("error al traer publicaciones");
+      }
+    );
+  }
 
-}//
+  // Método para formatear la fecha
+  formatDate(date: string | undefined): string {
+    if (!date) {
+      return ''; // Retorna un string vacío si la fecha es indefinida o nula
+    }
+    return this.datePipe.transform(date, 'dd/MM/yyyy') || ''; // Devuelve el formato de la fecha
+  }
+  
+}
